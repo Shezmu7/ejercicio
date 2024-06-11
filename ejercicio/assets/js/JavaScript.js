dbac8a2cd2edae92e20df7d72b288e535836c88a
Shezmu7 <xinlu.luis.barrientos.2003@gmail.com>
2024-06-11 09:08:00 -0400

    shesmu

--- ejercicio/assets/js/JavaScript.js
+++ ejercicio/assets/js/JavaScript.js
@@ -2,11 +2,16 @@
 //Variables y asignación
 
 let myName = "Luis";
-
-let myAge = "20";
-
+let myAge = 20;
 console.log(myName, myAge);
 
+//Tipos de Datos
 
+let numero : = 20;
+let texto: ="hola";
+let boleano : = true;
+console.log(numero);
+console.log(texto);
+console.log(boleano);
 
 
